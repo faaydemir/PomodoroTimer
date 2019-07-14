f4873c45b66678ebf75334e26af4163e26ee0e04
firakti <firakti@outlook.com>
2019-07-14 22:02:35 +0300

    app keys added

--- PomodoroTimer/PomodoroTimer/AppSecretsAndKeys.cs
+++ PomodoroTimer/PomodoroTimer/AppSecretsAndKeys.cs
@@ -5,10 +5,35 @@ using System.Text;
 namespace PomodoroTimer
 {
     /// <summary>
-    /// not commit when 
+    /// KeyStorage for get keys and secrets. it is initial state. after keys added will not commited see usage of get for needed keys.
     /// </summary>
-    public class AppSecretsAndKeys
+    internal class KeyStorage
     {
-        public static string FirebasePath = "https://<YourFireBaseAppPath>.firebaseio.com/";
+        private Dictionary<string, string> KeyDictionary;
+
+        public KeyStorage()
+        {
+            KeyDictionary = new Dictionary<string, string>();
+            //TODO Add needed keys 
+        }
+
+        public string Get(string Key)
+        {
+            if (KeyDictionary.ContainsKey(Key))
+            {
+                return KeyDictionary[Key];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+    public static class AppKeysAndSecrets
+    {
+        public static string Get(string key)
+        {
+            return new KeyStorage().Get(key);
+        }
     }
 }
